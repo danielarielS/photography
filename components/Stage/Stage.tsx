cc0d00963d8016c5458f85558f56aa35863141e0
daniel.ariel <daniel.ariel@brandung.de>
2022-01-18 12:46:38 +0100

    wip

--- components/Stage/Stage.tsx
+++ components/Stage/Stage.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
-
+import innsbruck_11 from "../../public/images/innsbruck_2021/innsbruck_11.jpeg";
 export const Stage = () => {
   return (
     <header className="">
-      <figure>
+      <figure className="w-80 h-80">
         <Image
-          src="/images/innsbruck_11"
+          src={innsbruck_11}
           layout="responsive"
-          width="100"
-          height="100"
+          width={1000}
+          height={1000}
           alt="Desert spring"
+          placeholder="blur"
         />
       </figure>
     </header>
